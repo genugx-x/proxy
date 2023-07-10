0bf52eae8e5cc828ef7c45e00f4938004190108c
genugx-x <qparanoya@gmail.com>
2023-07-10 15:29:55 +0900

    JDK 동적 프록시 예제 테스트 코드 작성

---
+++ src/test/java/com/genug/proxy/jdkdynamic/code/AInterface.java
@@ -0,0 +1,5 @@
+package com.genug.proxy.jdkdynamic.code;
+
+public interface AInterface {
+    String call();
+}
---
+++ src/test/java/com/genug/proxy/jdkdynamic/code/BInterface.java
@@ -0,0 +1,5 @@
+package com.genug.proxy.jdkdynamic.code;
+
+public interface BInterface {
+    String call();
+}
